05e858f6a09760fc49f5816dfdb98ccbcca96c6a
Hildeberto <hildeberto@gmail.com>
2026-06-06 12:21:23 -0300

    fix: configurar port da aplicação

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+
 import flet as ft
+from dotenv import load_dotenv
 
 from app.app import app_run
 
-ft.run(app_run)
+load_dotenv()
+
+ft.run(app_run, port=int(os.getenv("APP_SERVER_PORT", "8550")))
